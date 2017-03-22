d8ba55e513a189ce6d9aedb420d5846345f5c3cb
jack071000 <jack071000@naver.com>
2017-03-22 14:55:19 +0900

    오브젝트 클래스 구현

--- aquanautics/Object.cpp
+++ aquanautics/Object.cpp
@@ -6,7 +6,57 @@ Object::Object()
 {
 }
 
+Object::Object(std::string name) : Name(name), Parent(nullptr), Position(0.f, 0.f) , Rotation(0.f)
+{
+	printf("Object %s 积己凳\n", Name.c_str());
+}
+
 
 Object::~Object()
 {
+	for each(auto child in Children)
+		SAFE_DELETE(child);
+
+	Children.clear();
+
+	printf("Object %s 家戈凳\n", Name.c_str());
+
+}
+
+bool Object::Initialize()
+{
+	return false;
+}
+
+void Object::Release()
+{
+	for each(auto child in Children)
+		child->Release();
+
+	printf("Object %s 副府令凳\n", Name.c_str());
+
+}
+
+void Object::Update(float deltaTime)
+{
+	D3DXMatrixTransformation2D(&Matrix, NULL, 0.0f, NULL, NULL, Rotation, &Position);
+
+	if (Parent)
+		Matrix *= Parent->Matrix;
+
+	for each(auto child in Children)
+		child->Update(deltaTime);
+
+}
+
+void Object::Render()
+{
+	for each(auto child in Children)
+		child->Render();
+}
+
+void Object::AddChild(Object * child)
+{
+	child->Parent = this;
+	Children.push_back(child);
 }
--- aquanautics/Object.h
+++ aquanautics/Object.h
@@ -3,11 +3,30 @@ class Object
 {
 public:
 	Object* Parent;
-	
+	std::vector<Object*> Children;
 
+	std::string Name;
+
+	D3DXVECTOR2 Position;
+	FLOAT Rotation;
+
+	D3DXMATRIX Matrix;
 
 public:
 	Object();
+	Object(std::string name = "");
 	~Object();
+
+	virtual bool Initialize();
+
+	virtual void Release();
+
+	virtual void Update(float deltaTime);
+
+	virtual void Render();
+
+public:
+	void AddChild(Object* child);
+
 };
 
